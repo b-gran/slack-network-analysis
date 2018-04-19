f10dd96a66e399782584ee7a96f35603d1301749
b-gran <bill@billgranfield.com>
2018-04-20 00:04:49 +0100

    Pass in slack params + basic RPC impl

---
+++ src/api.js
@@ -0,0 +1,63 @@
+const R = require('ramda')
+const axios = require('axios')
+
+const models = require('./models')
+
+const SlackApi = token => ({
+  GetConversationHistory (conversationId, {
+    cursor,
+    start,
+    end,
+  } = {}) {
+    const startTimeSeconds = start && new Date(start).getTime() / 1000
+    const endTimeSeconds = end && new Date(end).getTime() / 1000
+    return axios({
+      url: 'https://slack.com/api/conversations.history',
+      params: {
+        token: token,
+        channel: conversationId,
+        cursor: cursor,
+        latest: endTimeSeconds,
+        oldest: startTimeSeconds,
+      }
+    })
+  },
+
+  GetAllUsers () {
+    return axios({
+      url: 'https://slack.com/api/users.list',
+      params: {
+        token: token,
+      }
+    })
+  }
+})
+
+const slack = SlackApi(process.env.SLACK_TOKEN)
+
+// Traverses all of the messages in the time frame
+module.exports.traverseMessages = async (conversationId, start, end) => {
+  let quota = 300
+  let moreMessages = true
+  let nextCursor = undefined
+  while (moreMessages && quota) {
+    const response = (await slack.GetConversationHistory(
+      conversationId,
+      { start, end, cursor: nextCursor, }
+    )).data
+    console.log(`GOT ${response.messages.length} MESSAGES`)
+    response.messages.forEach(message => console.log(`TS: ${message.ts}`))
+
+    moreMessages = response.has_more
+    nextCursor = moreMessages && response.response_metadata.next_cursor
+    console.log(`CURSOR ${nextCursor}`)
+  }
+}
+
+// Traverses all of the users in the slack team
+module.exports.traverseUsers = async () => {
+  const response = (await slack.GetAllUsers()).data
+  const users = response.members
+  const realUsers = users.filter(user => !user.deleted && !user.is_bot)
+  console.log(`GOT ${realUsers.length} USERS`)
+}
--- src/server.js
+++ src/server.js
@@ -1,30 +1,83 @@
 const http = require('http')
 const path = require('path')
+const express = require('express')
+const mongoose = require('mongoose')
+
+const Api = require('./api')
 
 console.log('Starting up...')
-
 process.once('SIGUSR2', () => {
   shutdown().then(() => {
     process.kill(process.pid, 'SIGUSR2')
   })
 })
 
+const SLACK_TOKEN = process.env.SLACK_TOKEN
+if (!SLACK_TOKEN) {
+  console.error('No slack token provided.')
+  process.exit(3)
+}
+
+const DATABASE_URL = process.env.DATABASE_URL
+if (!DATABASE_URL) {
+  console.error('No database URL')
+  process.exit(3)
+}
+console.log(`Connecting to mongodb at ${DATABASE_URL}`)
+
 let server
+const nextDirectory = path.join(__dirname, 'frontend')
 const next = require('next')
 const nextApp = next({
-  dir: path.join(__dirname, 'frontend'),
+  dir: nextDirectory,
   dev: process.env.NODE_ENV !== 'production',
 })
+console.log(`Serving next from ${nextDirectory}`)
 
-nextApp.prepare()
-  .then(() => {
+Promise.all([
+  nextApp.prepare(),
+  mongoose.connect(DATABASE_URL),
+])
+  .then(([nextInstance, mongooseConnection]) => {
     const handler = nextApp.getRequestHandler()
-    server = http.createServer((req, res) => {
-      console.log('req here')
+
+    const app = express()
+
+    app.get('/messages', (req, res) => {
+      console.log(`MESSAGES (${req.query.channel})`)
+      Api.traverseMessages(req.query.channel, new Date(2018, 2, 18, 19))
+        .then(result => {
+          console.log('result')
+          console.log(result)
+        })
+        .catch(err => {
+          console.log('err')
+          console.log(err)
+        })
+        .finally(() => res.end())
+    })
+
+    app.get('/users', (req, res) => {
+      console.log(`USERS (${req.query.channel})`)
+      Api.traverseUsers(req.query.channel)
+        .then(result => {
+          console.log('result')
+          console.log(result)
+        })
+        .catch(err => {
+          console.log('err')
+          console.log(err)
+        })
+        .finally(() => res.end())
+    })
+
+    app.use((req, res) => {
+      console.log('RENDER APP')
       return handler(req, res)
     })
 
-    server.listen(process.env.PORT)
+    server = http.createServer(app).listen(process.env.PORT)
+    console.log('Base server started')
   })
 
 async function shutdown () {
@@ -37,7 +90,7 @@ async function shutdown () {
 }
 
 function close (closeable) {
-  return new Promise(resolve => closeable.close(
+  return new Promise(resolve => closeable && closeable.close(
     err => {
       if (err) {
         console.log('Error closing')
