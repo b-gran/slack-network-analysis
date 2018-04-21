b2078a301697b331ea9e49a96e51329502a6ef4b
b-gran <bill@billgranfield.com>
2018-04-21 23:24:55 +0100

    Add Team model

---
+++ src/models.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+
+const teamSchema = mongoose.Schema({
+  token: {
+    type: String,
+    required: true
+  },
+  url: {
+    type: String,
+    required: true
+  },
+  team: {
+    type: String,
+    required: true
+  },
+  team_id: {
+    type: String,
+    required: true
+  },
+})
+
+module.exports.Team = mongoose.model('Team', teamSchema)
