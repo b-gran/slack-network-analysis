0be5169eecc958d39f9bd2eea837d48c502cce60
b-gran <bill@billgranfield.com>
2018-04-21 23:26:13 +0100

    Add team APIs to server

--- src/server.js
+++ src/server.js
@@ -2,6 +2,9 @@ const http = require('http')
 const path = require('path')
 const express = require('express')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const axios = require('axios')
+const httpError = require('./error').httpError
 
 const Api = require('./api')
 
@@ -43,6 +46,46 @@ Promise.all([
 
     const app = express()
 
+    app.use(bodyParser.json())
+
+    app.get('/slack/getTeamData', (req, res, next) => {
+      console.log('SLACK TEAM DATA')
+      if (!req.query.token) {
+        return res.statusCode(400).json(httpError(400, 'missing slack token'))
+      }
+
+      axios({
+        url: 'https://slack.com/api/auth.test',
+        params: {
+          token: req.query.token,
+        }
+      })
+        .then(res => res.statusCode(200).json(res.data))
+        .catch(err => res.statusCode(400).json(httpError(400, 'bad slack response', err)))
+    })
+
+    app.get('/teams', (req, res, next) => {
+      console.log('TEAMS')
+      Api.getTeams()
+        .then(result => {
+          console.log('result')
+          console.log(result)
+          return res.json(result)
+        })
+        .catch(next)
+    })
+
+    app.post('/teams', (req, res, next) => {
+      console.log(`CREATING TEAM ${req.body}`)
+      Api.createTeam(req.body)
+        .then(team => {
+          console.log('team')
+          console.log(team)
+          return res.json(team)
+        })
+        .catch(next)
+    })
+
     app.get('/messages', (req, res) => {
       console.log(`MESSAGES (${req.query.channel})`)
       Api.traverseMessages(req.query.channel, new Date(2018, 2, 18, 19))
@@ -74,6 +117,12 @@ Promise.all([
     app.use((req, res) => {
       console.log('RENDER APP')
       return handler(req, res)
+    })
+
+    app.use((err, req, res, next) => {
+      console.log('ERROR')
+      console.log(err)
+      return res.statusCode(500).json(err)
     })
 
     server = http.createServer(app).listen(process.env.PORT)
