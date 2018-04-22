f2f96faf0b9380736191f2ba774a50d4c99dd2b0
b-gran <bill@billgranfield.com>
2018-04-22 14:39:24 +0100

    Add User model

--- src/models.js
+++ src/models.js
@@ -1,5 +1,42 @@
 const mongoose = require('mongoose')
 
+const userSchema = mongoose.Schema({
+  // The user's ID within Slack
+  user_id: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+
+  // Users's real name
+  name: {
+    type: String,
+    required: true,
+  },
+
+  // Raw user model data from Slack
+  slack_data: {
+    type: Object,
+  },
+
+  // Which team (within SNA) this user belongs to
+  team: mongoose.SchemaTypes.ObjectId,
+})
+
+module.exports.User = mongoose.model('User', userSchema)
+
+const userData = mongoose.Schema({
+  has_user_data: {
+    type: Boolean,
+    default: false,
+  },
+  is_fetching: {
+    type: Boolean,
+    default: false,
+  },
+  last_fetched: Date,
+})
+
 const teamSchema = mongoose.Schema({
   token: {
     type: String,
@@ -9,14 +46,25 @@ const teamSchema = mongoose.Schema({
     type: String,
     required: true
   },
+
+  // Name of the team
   team: {
     type: String,
     required: true
   },
+
+  // Team's ID within slack
   team_id: {
     type: String,
     required: true,
     unique: true,
+  },
+
+  // Keep track of whether we've fetched the user data
+  user_data: {
+    type: userData,
+    required: true,
+    default: userData,
   },
 })
 
