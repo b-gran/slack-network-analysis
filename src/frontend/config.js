f6504ae5f8095f64896fea1fe69389000ce9f147
b-gran <bill@billgranfield.com>
2018-04-22 11:20:08 +0100

    Better support for HMR initial mobx state

--- src/frontend/config.js
+++ src/frontend/config.js
@@ -1,4 +1,22 @@
 import getConfig from 'next/config'
+import * as R from 'ramda'
+import { observable } from 'mobx'
 
 export const SERVER_URL = 'http://localhost:8080'
 export const SLACK_TOKEN = getConfig().publicRuntimeConfig.SLACK_TOKEN
+
+export const mergeInitialState = (initialState, prevState) => {
+  const sameKeys = R.equals(
+    new Set(Object.keys(initialState)),
+    new Set(Object.keys(prevState)),
+  )
+
+  if (sameKeys) {
+    return prevState
+  }
+
+  return observable(R.merge(
+    initialState,
+    prevState
+  ))
+}
