6f71ae7250796722d7bf7568b73a586be15e1f6e
b-gran <bill@billgranfield.com>
2018-04-24 22:41:39 +0100

    Add network models

--- src/models.js
+++ src/models.js
@@ -1,5 +1,13 @@
 const mongoose = require('mongoose')
 
+/************************************************
+ ************************************************
+ *******************         ********************
+ *********            SLACK            **********
+ *******************         ********************
+ ************************************************
+ ************************************************/
+
 const userSchema = mongoose.Schema({
   // The user's ID within Slack
   user_id: {
@@ -20,7 +28,11 @@ const userSchema = mongoose.Schema({
   },
 
   // Which team (within SNA) this user belongs to
-  team: mongoose.SchemaTypes.ObjectId,
+  team: {
+    type: mongoose.SchemaTypes.ObjectId,
+    ref: 'Team',
+    required: true,
+  },
 })
 
 module.exports.User = mongoose.model('User', userSchema)
@@ -44,10 +56,126 @@ const channelSchema = mongoose.Schema({
   },
 
   // Which team (within SNA) this channel belongs to
-  team: mongoose.SchemaTypes.ObjectId,
+  team: {
+    type: mongoose.SchemaTypes.ObjectId,
+    ref: 'Team',
+    required: true,
+  },
 })
 
 module.exports.Channel = mongoose.model('Channel', channelSchema)
+
+const messageSchema = mongoose.Schema({
+  user_id: {
+    type: String,
+    required: true,
+  },
+
+  ts: {
+    type: String,
+    required: true,
+  },
+
+  text: String,
+
+  reactions: mongoose.SchemaTypes.Array,
+
+  replies: mongoose.SchemaTypes.Array,
+
+  channel: {
+    type: String,
+    required: true,
+  },
+
+  team: {
+    type: mongoose.SchemaTypes.ObjectId,
+    ref: 'Team',
+    required: true,
+  },
+})
+
+module.exports.Message = mongoose.model('Message', messageSchema)
+
+/************************************************
+ ************************************************
+ *******************         ********************
+ ********            NETWORK            *********
+ *******************         ********************
+ ************************************************
+ ************************************************/
+
+// A way to group different networks generated
+// from the same Slack data.
+const graphSchema = mongoose.Schema({
+  description: String,
+
+  team: {
+    type: mongoose.SchemaTypes.ObjectId,
+    ref: 'Team',
+    required: true,
+  },
+})
+module.exports.Graph = mongoose.model('Graph', graphSchema)
+
+const nodeSchema = mongoose.Schema({
+  graph: {
+    type: mongoose.SchemaTypes.ObjectId,
+    ref: 'Graph',
+    required: true,
+  },
+
+  team: {
+    type: mongoose.SchemaTypes.ObjectId,
+    ref: 'Team',
+    required: true,
+  },
+
+  user: {
+    type: mongoose.SchemaTypes.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+})
+module.exports.Node = mongoose.model('Node', nodeSchema)
+
+const edgeSchema = mongoose.Schema({
+  graph: {
+    type: mongoose.SchemaTypes.ObjectId,
+    ref: 'Graph',
+    required: true,
+  },
+  
+  team: {
+    type: mongoose.SchemaTypes.ObjectId,
+    ref: 'Team',
+    required: true,
+  },
+
+  // The two nodes (ids correspond to Node).
+  // Should always be an array of length two.
+  vertices: {
+    type: [{
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: 'Node',
+    }],
+    required: true,
+  },
+
+  // Required to be a weighted graph.
+  weight: {
+    type: mongoose.SchemaTypes.Number,
+    required: true,
+  },
+})
+module.exports.Edge = mongoose.model('Edge', edgeSchema)
+
+/************************************************
+ ************************************************
+ *******************         ********************
+ ********             META             **********
+ *******************         ********************
+ ************************************************
+ ************************************************/
 
 const jobData = mongoose.Schema({
   ever_run: {
@@ -104,36 +232,23 @@ const teamSchema = mongoose.Schema({
     required: true,
     default: jobData,
   },
+
+  // Keep track of whether we've generated network data
+  network_job: {
+    type: jobData,
+    required: true,
+    default: jobData,
+  },
+
+  networks: {
+    type: [{
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: 'Network',
+    }],
+    required: true,
+    default: [],
+  }
 })
 
 module.exports.Team = mongoose.model('Team', teamSchema)
 
-const messageSchema = mongoose.Schema({
-  user_id: {
-    type: String,
-    required: true,
-  },
-
-  ts: {
-    type: String,
-    required: true,
-  },
-
-  text: String,
-
-  reactions: mongoose.SchemaTypes.Array,
-
-  replies: mongoose.SchemaTypes.Array,
-
-  channel: {
-    type: String,
-    required: true,
-  },
-
-  team: {
-    type: mongoose.SchemaTypes.ObjectId,
-    required: true,
-  },
-})
-
-module.exports.Message = mongoose.model('Message', messageSchema)
